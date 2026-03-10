9acb0946e7c5fe0ce0d3fcb4976b3f2fd6b446ab
Víctor <pixelkillermail@gmail.com>
2026-03-10 19:48:37 +0100

    Day 2 update

--- src/main/java/com/kata/FizzBuzz.java
+++ src/main/java/com/kata/FizzBuzz.java
@@ -1,16 +1,49 @@
 package com.kata;
+import java.util.*;
 
 public class FizzBuzz
 {
+    private int fizzNumber = 3;
+    private int buzzNumber = 5;
+
     public String check( int number )
     {
         String response = "";
-        int fizzNumber = 3;
 
-        if ( number % 3 == 0 ) response += "Fizz";
-        if ( number % 5 == 0 ) response += "Buzz";
-        if ( number % 3 != 0 &&  number % 5 != 0  ) response = String.valueOf(number);
+        if (isFizz(number)) response += "Fizz";
+        if (isBuzz(number)) response += "Buzz";
+        if (isFizzBuzz(number)) response = String.valueOf(number);
 
         return response;
     }
+
+    private boolean isFizz(int number) {
+        return number % fizzNumber == 0;
+    }
+
+    private boolean isBuzz(int number) {
+        return number % buzzNumber == 0;
+    }
+
+    private boolean isFizzBuzz(int number) {
+        return number % fizzNumber != 0 && number % buzzNumber != 0;
+    }
+
+    public String getList() {
+        String listAsString = "";
+        int firstListNumber = 1; 
+        int lastListNumber = 100;
+
+        for (int count = firstListNumber; count <= lastListNumber; count ++) {
+
+            listAsString += count + " ";
+        }
+
+        return listAsString.trim();
+    }
+
+    private String chop (String kko) {
+        return "";
+    }
+
 }
